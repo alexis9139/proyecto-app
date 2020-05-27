6bde24a26259abb05c92da9a3f20e24942933725
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-05-27 11:13:08 -0300

    breakpoints, agregacion de iconos a BarSession

--- src/componentes/layout/bar/BarSession.js
+++ src/componentes/layout/bar/BarSession.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { Toolbar, Typography, Button } from '@material-ui/core';
+import { Toolbar, Typography, Button, IconButton } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';//esto debe agregarse como funcionalidad
-
 const styles = theme => ({
     sectionDesktop: {
         display: "none",
         [theme.breakpoints.up("md")]: {
             display: "flex"
         }
-    }
+    },
+    grow: {//para que se muestre a la derecha
+        flexGrow: 1
+    },
+    sectionMobile: {
+        display: "flex",
+        [theme.breakpoints.up("md")]: {
+            display: "none"
+        }
+    },
 });
 
 export class BarSession extends Component {
@@ -17,13 +25,23 @@ export class BarSession extends Component {
         return (
             <div>
                 <Toolbar>
+                    <IconButton color="inherit">
+                        <i className="material-icons">menu</i>
+                    </IconButton>
                     {/* Typography es para escribir texto */}
                     <Typography
                         variant="h6">
                         ALEX HOME
                     </Typography>
+                    <div className={classes.grow}></div>
                     <div className={classes.sectionDesktop}>
-                        <Button>Login</Button>
+                        {/* color="inherit" toma los colores heredados */}
+                        <Button color="inherit">Login</Button>
+                    </div>
+                    <div className={classes.sectionMobile}>
+                        <IconButton color="inherit">
+                            <i className="material-icons">more_vert</i>
+                        </IconButton>
                     </div>
                 </Toolbar>
             </div>
