e7c040c890a7ca97dfadc79600aaf2749823cca9
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-05-28 10:20:52 -0300

    Maquetacion de Login.js

--- src/componentes/seguridad/Login.js
+++ src/componentes/seguridad/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Avatar, Typography } from '@material-ui/core'
+import { Container, Avatar, Typography, TextField, Button } from '@material-ui/core'
 import LockOutlineIcon from '@material-ui/icons/LockOutlined';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 
@@ -13,6 +13,11 @@ const style = {
     avatar: {
         margin: 5,
         backgroundColor: "red"
+    },
+    form: {
+        width: "100%",
+        marginTop: 8,
+
     }
 }
 
@@ -29,6 +34,32 @@ class Login extends Component {
                     <Typography component="h1" variant="h5">
                         Ingrese usuario
                     </Typography>
+                    <form styl={style.form}>
+                        <TextField
+                            variant="outlined"//borde
+                            label="Email"
+                            name="email"
+                            fullWidth
+                            margin="normal"
+                        />
+
+                        <TextField
+                            variant="outlined"//borde
+                            label="Password"
+                            name="password"
+                            type="password"
+                            fullWidth
+                            margin="normal"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"//le da un color de fondo
+                            color="primary"
+                        >
+                            Enviar
+                        </Button>
+                    </form>
                 </div>
             </Container>
         )
