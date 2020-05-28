0de11fb25e03d36b2584ee3d44bfadb40d90662e
Medrano Alexis Bruno Baloy <alexismedrano9139@gmail.com>
2020-05-27 21:50:07 -0300

    creacion de carpeta server, y el archivo firebase.js el cual tiene la configuracion de la app

---
+++ src/server/firebase.js
@@ -0,0 +1,22 @@
+import app from './firebase/app';
+
+const config = {
+    apiKey: "AIzaSyD15XmzDQPBuhzU-10UX4Iwzso3UKTqslQ",
+    authDomain: "gestiones-8f421.firebaseapp.com",
+    databaseURL: "https://gestiones-8f421.firebaseio.com",
+    projectId: "gestiones-8f421",
+    storageBucket: "gestiones-8f421.appspot.com",
+    messagingSenderId: "622423835021",
+    appId: "1:622423835021:web:d61d5ae0fa2ea058d07dc4",
+    measurementId: "G-7W1VBCXR1T"
+};
+
+class Firebase {
+    constructor() {
+        //invocamos al objeto firebase y lo inicializamos
+        //como parametro el pasamos la configuracion de la app web que creamos
+        app.initializeApp(config);
+        this.db = app.firestore();
+    }
+}
+export default Firebase;
